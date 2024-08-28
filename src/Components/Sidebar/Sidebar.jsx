259ff2af1aead8887e61f92ff0c324d4868cafd6
Navya07122001 <navyasreeparvatham@gmail.com>
2024-08-28 17:18:57 +0530

    implemented chat section

---
+++ src/Components/Sidebar/Sidebar.jsx
@@ -0,0 +1,110 @@
+import React from 'react';
+import {
+    Box,
+    Container,
+    Button,
+    List,
+    ListItem,
+    Stack,
+    Typography,
+    useMediaQuery,
+    IconButton,
+  } from "@mui/material";
+import { useContext } from 'react';
+//   import MenuIcon from '@mui/material/Menu'
+import styles from "./Sidebar.module.css";
+import newchat from '../../assets/newchat.png'
+import logo from '../../assets/logo.png';
+import { ThemeContext } from '../../theme/ThemeContext';
+import { Link } from 'react-router-dom';
+import { useState } from "react";
+import AddCommentIcon from '@mui/icons-material/AddComment';
+import CloseIcon from "@mui/icons-material/Close";
+import MenuIcon from "@mui/icons-material/Menu";
+
+function Sidebar({setChat,closeMenu}) {
+  const {mode,setMode}=useContext(ThemeContext)
+
+    const isMobile=useMediaQuery("(max-width:800px)");
+   const handleMode=()=>{
+    setMode(prev=>prev==='light'?'dark':'light')
+   }
+  
+    return (
+      <Box >
+
+
+      {isMobile && (
+          <Button
+              endIcon={<CloseIcon />}
+              sx={{
+                  width: 1,
+                  justifyContent: 'flex-end',
+                  color: mode == 'light' ? 'primary.dark' :'text.primary'
+              }}
+              onClick={closeMenu}
+          >
+           
+          </Button>
+      )}
+
+      <Link to={'/'} style={{ textDecoration: 'none' }}>
+          <Stack
+              onClick={() => {
+                  setChat([])
+                  closeMenu()
+              }}
+              sx={{
+                  bgcolor: 'primary.main',
+                  '&:hover ': {
+                      bgcolor: 'primary.bg'
+                  }
+              }}
+              direction={'row'}
+              spacing={1}
+              alignItems={'center'}
+              justifyContent={'space-between'}
+              py={2}
+              px={{xs:2, md:3}}
+          >
+              <Stack direction={'row'} gap={1} alignItems={'center'}>
+                  <Box
+                      component={'img'}
+                      src={logo}
+                      height={42}
+                      width={42}
+                      borderRadius={2}
+                      boxShadow={4}
+                      flexShrink={0}
+                  />
+                  <Typography
+                      variant={'heading'}
+                      fontSize={{xs:16, md:20}}
+                      color={'text.primary'}
+                  >
+                      New Chat
+                  </Typography>
+              </Stack>
+
+              <AddCommentIcon sx={{ color: 'text.primary' }} />
+
+          </Stack>
+      </Link>
+
+      <Box p={{xs:2, md:3}}>
+          <Link to={'/history'}>
+              <Button
+                  variant="contained" sx={{ width: 1 }}
+                  onClick={closeMenu}
+              >
+                  Past Conversations
+              </Button>
+          </Link>
+      </Box>
+
+  </Box>
+
+    );
+}
+
+export default Sidebar;
